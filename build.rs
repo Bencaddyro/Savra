b8248c43ca93c2b13ce0aa8fb74cd6a924fe2193
BenCaddyro <bencaddyro@protonmail.com>
2022-05-30 23:43:59 +0200

    build.rs refactoring
    Complete refactoring of build.rs with cleaner structuration of YAML parsing and data.rs writing

--- build.rs
+++ build.rs
@@ -9,13 +9,14 @@ use convert_case::{Case, Casing};
 use glob::glob;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-struct Destination {
+struct YamlDestination {
     location: String,
     distance: f64,
+    bidirection: Option<f64>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-struct Goods {
+struct YamlCommodities {
     product: String,
     price: f64,
     flow: Option<usize>,
@@ -23,100 +24,23 @@ struct Goods {
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-struct Entry {
+struct YamlEntry {
     location: String,
-    destination: Vec<Destination>,
-    buy: Option<Vec<Goods>>,
-    sell: Option<Vec<Goods>>,
+    destination: Option<Vec<YamlDestination>>,
+    buy: Option<Vec<YamlCommodities>>,
+    sell: Option<Vec<YamlCommodities>>,
 }
-
-fn read_yaml(path: &str) -> Vec<Entry> {
-    let contents = fs::read_to_string(path).expect(&format!("Unable to read {}",path));
-    serde_yaml::from_str::<Vec<Entry>>(&contents).expect(&format!("Err to parse {}",path))
-}
-
-
-fn write_data(file: &mut File, location: BTreeSet<String>, product_info: HashMap<String,InfoProd>, get_map: String, get_buy: String, get_sell: String) -> std::io::Result<()> {
-
-    file.write(b"use std::collections::HashMap;\n")?;
-    file.write(b"use strum::EnumString;\n")?;
-
-    let product = product_info.keys().map(|x| x.to_owned()).collect::<Vec<String>>();
-    
-    // Enum Location
-    file.write(b"\n// Auto-generated Location\n")?;
-    file.write(b"#[derive(Clone, Copy, Hash, Eq, PartialOrd, PartialEq, Debug, EnumString)]\n")?;
-    
-    let s = format!("pub enum Location {{\n  {}\n}}",location.into_iter().collect::<Vec<String>>().join(",\n  "));
-    file.write(s.as_bytes())?;
-    
-    // Enum Product
-    file.write(b"\n\n// Auto-generated Product\n")?;
-    file.write(b"#[derive(Clone, Copy, Hash, Eq, PartialOrd, PartialEq, Debug, EnumString)]\n")?;
-    //let s = format!("pub enum Product {{\n  {}\n}}", product_info.clone().into_keys().collect::<Vec<String>>().join(",\n  "));
-    let s = format!("pub enum Product {{\n  {}\n}}", product.clone().join(",\n  "));
-    file.write(s.as_bytes())?;
-    
-    // impl Product
-    file.write(b"\n\nimpl Product {\n")?;
-    
-    // fn min / max
-    let mut s_min = "pub fn min(&self) -> f64 {\nmatch self {\n".to_owned();
-    let mut s_max = "pub fn max(&self) -> f64 {\nmatch self {\n".to_owned();
-        
-    for (p,InfoProd(min,max)) in &product_info {
-        
-        s_min += &format!("Product::{} => {} as f64,\n",p,min);
-        s_max += &format!("Product::{} => {} as f64,\n",p,max);
-    
-    }
-    //s_min += "_ => 0.0,\n}}\n";
-    //s_max += "_ => 0.0,\n}}\n";
-    
-    s_min += "}}\n";
-    s_max += "}}\n";
-    
-    file.write(s_min.as_bytes())?;
-    file.write(s_max.as_bytes())?;
-    
-    // fn all_product
-    file.write(format!("pub fn all() -> [Product;{}] {{\n",&product_info.len()).as_bytes())?;
-    //let s = format!("[Product::{}\n]}}\n",product_info.clone().into_keys().collect::<Vec<String>>().join(",\nProduct::"));
-    let s = format!("[Product::{}\n]}}\n",product.clone().join(",\nProduct::"));
-    file.write(s.as_bytes())?;
-    
-    //end Enum Product
-    file.write(b"}\n")?;
-    
-    // fn get_map
-    file.write(b"\n\n// Auto-generated get_map()\n")?;
-    file.write(get_map.as_bytes())?;
-    // fn get_buy
-    file.write(b"\n\n// Auto-generated get_buy()\n")?;
-    file.write(get_buy.as_bytes())?;
-    // fn get_sell
-    file.write(b"\n\n// Auto-generated get_sell()\n")?;
-    file.write(get_sell.as_bytes())?;
-    
-    file.write(b"\n\n")?;
-    Ok(())
-}
-
-fn loc(s: String, v: f64) -> String { format!("\t(Location::{}, {} as f64),\n",s,v) }
-fn prod(s: String, v: f64) -> String { format!("\t(Product::{}, {} as f64),\n",s,v) }
 
 #[derive(Clone)]
 struct InfoProd(f64,f64);
 
 
 fn main() {
-
-    // Info for source *.yml
     println!("cargo:rerun-if-changed=data_model");
+
+    // Import data from *.yml
     let input = "data_model/**/*.yml";
-    
-    // Import data from *.yml 
-    let mut data = Vec::new();
+    let mut data: Vec<YamlEntry> = Vec::new();
     for entry in glob(input).unwrap() {
         if let Ok(path) = entry {
             let mut d = read_yaml(path.to_str().unwrap());
@@ -124,76 +48,172 @@ fn main() {
         }
     }
 
-    // Info for .rs output
+    // Process data
+    let mut location: BTreeSet<String> = BTreeSet::new();
+    let mut location_control: BTreeSet<String> = BTreeSet::new();
+    let mut product_info: HashMap<String, InfoProd> = HashMap::new();
+    let mut get_map: HashMap<String, HashMap<String, f64>> = HashMap::new();
+    let mut get_buy: HashMap<String, HashMap<String, f64>> = HashMap::new();
+    let mut get_sell: HashMap<String, HashMap<String, f64>> = HashMap::new();
+    process(data, &mut location, &mut location_control, &mut product_info, &mut get_map, &mut get_buy, &mut get_sell);
+
+    // Check data
+    assert!(location.symmetric_difference(&location_control).cloned().collect::<Vec<_>>().is_empty(), "Destination contains unknow location");
+
+    // Write data
     let output = "src/data.rs";
     let mut file = File::create(output).expect(&format!("Unable to open {}",output));
-    
-    // Enum set for Location & Product(min max)
-    let mut location:BTreeSet<String> = BTreeSet::new();
-    let mut location_control:BTreeSet<String> = BTreeSet::new();
-    //let mut product:BTreeSet<InfoProd> = BTreeSet::new();
-    let mut product_info:HashMap<String,InfoProd> = HashMap::new();
-    
-    // get header
-    let mut get_map = "pub fn get_map() -> HashMap<Location,Vec<(Location,f64)>> {[\n".to_owned();
-    let mut get_buy = "pub fn get_buy() -> HashMap<Location,HashMap<Product,f64>> {[\n".to_owned();
-    let mut get_sell = "pub fn get_sell() -> HashMap<Location,HashMap<Product,f64>> {[\n".to_owned();
-    
-    for e in data {
-        location.insert(e.location.clone().to_case(Case::Pascal));
-        
-        // get_map
-        get_map.push_str(&format!("(Location::{},[\n",e.location.to_case(Case::Pascal)));
-        for d in e.destination {
-            location_control.insert(d.location.clone().to_case(Case::Pascal));
-            get_map.push_str(&loc(d.location.to_case(Case::Pascal), d.distance));
-        }
-        get_map.push_str("].iter().cloned().collect()),\n");
-        
-        // get_buy
-        if e.buy.is_some() {
-            get_buy.push_str(&format!("(Location::{},[\n",e.location.to_case(Case::Pascal)));
-            for g in e.buy.as_ref().unwrap() {
-                get_buy.push_str(&prod(g.product.clone().to_case(Case::Pascal), g.price));
+    write_data(&mut file, location, product_info, get_map, get_buy, get_sell).unwrap();
+}
 
-                // register / update new product
-                let old = product_info.insert(g.product.clone().to_case(Case::Pascal),InfoProd(g.price,0.0));
-                match old {
-                    None => (),
-                    Some(InfoProd(min,max)) => {product_info.insert(g.product.clone().to_case(Case::Pascal),InfoProd(g.price.min(min),max));()}
+
+fn read_yaml(path: &str) -> Vec<YamlEntry> {
+    let contents = fs::read_to_string(path).expect(&format!("Unable to read {}",path));
+    serde_yaml::from_str::<Vec<YamlEntry>>(&contents).expect(&format!("Err to parse {}",path))
+}
+
+
+fn process(data: Vec<YamlEntry>, location_enum: &mut BTreeSet<String>, location_control: &mut BTreeSet<String>, product_info: &mut HashMap<String, InfoProd>, get_map: &mut HashMap<String, HashMap<String, f64>>, get_buy: &mut HashMap<String, HashMap<String, f64>>, get_sell: &mut HashMap<String, HashMap<String, f64>>){
+    for e in data {
+        let location = e.location.to_case(Case::Pascal);
+
+        // Register location entry
+        location_enum.insert(location.clone());
+
+        // Get destination
+        let mut data_map: HashMap<String,f64> = HashMap::new();
+        let mut bidirection: Option<(String,f64)> = None;
+
+        if e.destination.is_some() {
+            for d in e.destination.unwrap_or_default() {
+                let destination = d.location.to_case(Case::Pascal);
+                location_control.insert(destination.clone());
+                data_map.insert(destination, d.distance);
+                if d.bidirection.is_some() {
+                    ()
                 }
-                
             }
-            get_buy.push_str("].iter().cloned().collect()),\n");
+            get_map.insert(location.clone(), data_map);
         }
-        
-        // get_sell
+        // need function try to create, if already in hasmap, to add new value
+        // updta location control with destination
+        if bidirection.is_some() {
+            ()
+        }
+
+
+        // Get buy
         if e.buy.is_some() {
-            get_sell.push_str(&format!("(Location::{},[\n",e.location.to_case(Case::Pascal)));
-            for g in e.sell.as_ref().unwrap() {
-                get_sell.push_str(&prod(g.product.clone().to_case(Case::Pascal), g.price));
-                
-                // register / update new product
-                let old = product_info.insert(g.product.clone().to_case(Case::Pascal),InfoProd(0.0,g.price));
-                match old {
-                    None => (),
-                    Some(InfoProd(min,max)) => {product_info.insert(g.product.clone().to_case(Case::Pascal),InfoProd(min,g.price.max(max)));()}
+            let mut data_map: HashMap<String,f64> = HashMap::new();
+            for g in e.buy.unwrap_or_default() {
+                let product = g.product.to_case(Case::Pascal);
+                data_map.insert(product.clone(), g.price);
+                if let Some(InfoProd(min, max)) = product_info.insert(product.clone(), InfoProd(g.price,0.0)) {
+                    product_info.insert(product.clone(), InfoProd(g.price.min(min), max));
                 }
-                                
             }
-            get_sell.push_str("].iter().cloned().collect()),\n");
+            get_buy.insert(location.clone(), data_map);
+        }
+
+        // Get sell
+        if e.sell.is_some() {
+            let mut data_map: HashMap<String,f64> = HashMap::new();
+            for g in e.sell.unwrap_or_default() {
+                let product = g.product.to_case(Case::Pascal);
+                data_map.insert(product.clone(), g.price);
+                if let Some(InfoProd(min, max)) = product_info.insert(product.clone(), InfoProd(g.price,0.0)) {
+                    product_info.insert(product.clone(), InfoProd(min, g.price.max(max)));
+                }
+            }
+            get_sell.insert(location.clone(), data_map);
         }
     }
-    
-    // get footer
-    get_map.push_str("].iter().cloned().collect()}");
-    get_buy.push_str("].iter().cloned().collect()}");
-    get_sell.push_str("].iter().cloned().collect()}");
-    
-    // Control Location closure
-    assert!(location.symmetric_difference(&location_control).cloned().collect::<Vec<_>>().is_empty(), "Destination contains unknow location");
-    
-    // Write to output
-    write_data(&mut file, location, product_info, get_map, get_buy, get_sell).unwrap();
-    
 }
+
+
+fn write_data(
+    file: &mut File,
+    location: BTreeSet<String>,
+    product_info: HashMap<String,InfoProd>,
+    get_map: HashMap<String, HashMap<String, f64>>,
+    get_buy: HashMap<String, HashMap<String, f64>>,
+    get_sell: HashMap<String, HashMap<String, f64>>
+    ) -> std::io::Result<()> {
+
+    // data.rs use
+    file.write(b"use std::collections::HashMap;\n")?;
+    file.write(b"use strum::EnumString;\n")?;
+
+    // Enum Location
+    file.write(b"\n// Auto-generated Location\n")?;
+    file.write(b"#[derive(Clone, Copy, Hash, Eq, PartialOrd, PartialEq, Debug, EnumString)]\n")?;
+    file.write(b"pub enum Location {\n")?;
+    for l in location {
+        file.write(format!("\t{},\n", l).as_bytes())?;
+    }
+    file.write(b"}\n")?;
+
+    // Enum Product
+    file.write(b"\n// Auto-generated Product\n")?;
+    file.write(b"#[derive(Clone, Copy, Hash, Eq, PartialOrd, PartialEq, Debug, EnumString)]\n")?;
+    file.write(b"pub enum Product {\n")?;
+    for (p, ..) in product_info.clone() {
+        file.write(format!("\t{},\n", p).as_bytes())?;
+    }
+    file.write(b"}\n")?;
+
+    // Product impl
+    file.write(b"\n// Auto-generated impl Product\n")?;
+    file.write(b"impl Product {\n")?;
+
+    // Product max
+    file.write(b"\tpub fn max(&self) -> f64 {\n\t\tmatch self {\n")?;
+    for (p, InfoProd(min,max)) in product_info {
+        file.write(format!("\t\t\tProduct::{} => {} as f64,\n", p, max).as_bytes())?;
+    }
+    file.write(b"\t\t}\n\t}\n")?;
+
+    file.write(b"}\n")?;
+
+
+
+    // GET_MAP
+    file.write(b"\n// // Auto-generated get_map()\n")?;
+    file.write(b"pub fn get_map() -> HashMap<Location, HashMap<Location, f64>> {[\n")?;
+    for (location, vec_destination) in get_map {
+        file.write(format!("\t(Location::{}, [\n", location).as_bytes())?;
+        for (destination, distance) in vec_destination {
+            file.write(format!("\t\t(Location::{}, {} as f64),\n", destination, distance).as_bytes())?;
+        }
+        file.write(b"\t\t].iter().cloned().collect()),\n")?;
+    }
+    file.write(b"\t].iter().cloned().collect()}\n")?;
+
+    // GET_BUY
+    file.write(b"\n// // Auto-generated get_buy()\n")?;
+    file.write(b"pub fn get_buy() -> HashMap<Location, HashMap<Product, f64>> {[\n")?;
+    for (location, vec_product) in get_buy {
+        file.write(format!("\t(Location::{}, [\n", location).as_bytes())?;
+        for (product, price) in vec_product {
+            file.write(format!("\t\t(Product::{}, {} as f64),\n", product, price).as_bytes())?;
+        }
+        file.write(b"\t\t].iter().cloned().collect()),\n")?;
+    }
+    file.write(b"\t].iter().cloned().collect()}\n")?;
+
+    // GET_SELL
+    file.write(b"\n// // Auto-generated get_sell()\n")?;
+    file.write(b"pub fn get_sell() -> HashMap<Location, HashMap<Product, f64>> {[\n")?;
+    for (location, vec_product) in get_sell {
+        file.write(format!("\t(Location::{}, [\n", location).as_bytes())?;
+        for (product, price) in vec_product {
+            file.write(format!("\t\t(Product::{}, {} as f64),\n", product, price).as_bytes())?;
+        }
+        file.write(b"\t\t].iter().cloned().collect()),\n")?;
+    }
+    file.write(b"\t].iter().cloned().collect()}\n")?;
+    Ok(())
+}
+
+
+
